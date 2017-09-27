9b333c3ee80589fba9e53db5b4b3be1e84feea0b
Agnieszka Cieśla <agnieszka.ciesla333@gmail.com>
2017-09-27 13:36:49 +0200

    Mobile navigation done.

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
-  selector: 'navigation',
-  templateUrl: './navigation.component.html',
-  styleUrls: [
-      './navigation-container.scss',
-      './nav-icon-toggler.scss',
-      './navigation.component.scss'
-  ]
+    selector: 'navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: [
+        './navigation-container.scss',
+        './nav-icon-toggler.scss',
+        './navigation.component.scss'
+    ]
 })
 export class NavigationComponent {
-  public isOpen: boolean = false;
-  public programAvailable: boolean = false;
+    @ViewChild('navbarToggler') navbarToggler: ElementRef;
+    public isOpen: boolean = false;
+    public programAvailable: boolean = false;
+
+    public toggle() {
+        this.isOpen = !this.isOpen;
+        console.log(this.isOpen);
+    }
+
+    public toggleNav() {
+        this.navbarToggler.nativeElement.click();
+    }
 }
